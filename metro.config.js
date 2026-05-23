d62faf48e352044b97644a40997771c1c2541f8a
anasvalli <anasvalli3@gmail.com>
2026-05-24 03:15:33 +0500

    Fix Hermes build: mock @opentelemetry/api to resolve dynamic import error

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,11 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (moduleName === "@opentelemetry/api") {
+    return { type: "empty" };
+  }
+  return context.resolveRequest(context, moduleName, platform);
+};
+
 module.exports = withNativeWind(config, { input: "./global.css" });
